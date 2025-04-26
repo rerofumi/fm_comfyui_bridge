05dca239a5d3e20ffe80b3ac6f8386b48b96cf9e
rerofumi <rero2@yuumu.org>
2025-04-26 20:31:36 +0900

    fix lora ignore

--- src/fm_comfyui_bridge/bridge.py
+++ src/fm_comfyui_bridge/bridge.py
@@ -220,8 +220,8 @@ def t2i_highreso_request_build(
     prompt_path[config.COMFYUI_NODE_HR_SIZE_HEIGHT]["inputs"]["value"] = image_size[1]
     for node in config.COMFYUI_NODE_HR_LORA_CHECKPOINT:
         prompt_path[node[0]]["inputs"]["lora_name"] = lora.model
-        prompt_path[node[0]]["inputs"]["strength_model"] = lora.strength
-        prompt_path[node[0]]["inputs"]["strength_clip"] = lora.strength
+        prompt_path[node[0]]["inputs"]["strength_model"] = lora.strength * node[1]
+        prompt_path[node[0]]["inputs"]["strength_clip"] = lora.strength * node[1]
     current_date = datetime.datetime.now().strftime("%Y-%m-%d")
     prompt_path[config.COMFYUI_NODE_HR_OUTPUT]["inputs"]["filename_prefix"] = (
         f"{current_date}/Bridge"
@@ -262,8 +262,8 @@ def i2i_highreso_request_build(
         prompt_path[node]["inputs"]["seed"] = random.randint(1, 10000000000)
     for node in config.COMFYUI_NODE_HR_LORA_CHECKPOINT:
         prompt_path[node[0]]["inputs"]["lora_name"] = lora.model
-        prompt_path[node[0]]["inputs"]["strength_model"] = lora.strength
-        prompt_path[node[0]]["inputs"]["strength_clip"] = lora.strength
+        prompt_path[node[0]]["inputs"]["strength_model"] = lora.strength * node[1]
+        prompt_path[node[0]]["inputs"]["strength_clip"] = lora.strength * node[1]
     current_date = datetime.datetime.now().strftime("%Y-%m-%d")
     prompt_path[config.COMFYUI_NODE_HR_OUTPUT]["inputs"]["filename_prefix"] = (
         f"{current_date}/Bridge"
